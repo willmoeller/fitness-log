63d58a8022e415a5df171540640db02957dbd1fb
Will Moeller <willmoeller@gmail.com>
2023-04-26 00:31:32 +0000

    Set up basic layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def index
+    render({ :template => "index.html.erb" })
+  end
+
+  def workouts
+    render({ :template => "workouts.html.erb" })
+  end
 end
